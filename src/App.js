a0c8f25a30d599bd5e27c0ced339b808b9dd87c0
isoboye24 <isobarguy4real@gmail.com>
2021-09-06 11:25:44 +0300

    Lorem-ipsum text app

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>Tired of boring Ipsum Lorem?</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amount">Paragraph:</label>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          value={count}
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
